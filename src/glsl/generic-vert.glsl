99a96536b22a71ed372eea65db8eaa3985a4b52c
ac101m <mofty101@gmail.com>
2019-02-22 15:33:46 +0000

    Add model view projection matrix to vertex shader

--- src/glsl/generic-vert.glsl
+++ src/glsl/generic-vert.glsl
@@ -5,10 +5,13 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 uv;
 
+// Uniforms
+uniform mat4 mvp;
+
 // Outputs from the vertex shader
 out vec4 colour;
 
 void main() {
-  gl_Position = vec4(position, 1.0);
+  gl_Position = mvp * vec4(position, 1.0);
   colour = vec4(normal, 1.0);
 }
