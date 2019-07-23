c76dee8192903906ee0a8c09bb6d5fcd6f959c84
ac101m <mofty101@gmail.com>
2019-07-23 22:19:57 +0100

    Add buffer class

---
+++ include/GLT/Buffer.hpp
@@ -0,0 +1,83 @@
+#ifndef GLT_BUFFER_INCLUDED
+#define GLT_BUFFER_INCLUDED
+
+
+// This project
+#ifndef GLT_MASTER_INCLUDED
+#include <GLT/Master.hpp>
+#endif
+#include <GLT/RefCount.hpp>
+#include <GLT/ElementStack.hpp>
+
+
+// Standard
+#include <vector>
+
+
+namespace GLT {
+
+  // Class wraps opengl buffer construct
+  class Buffer : public RefCount {
+  private:
+
+    // Underlying opengl handle
+    GLuint glHandle;
+
+    // Default buffer usage hint
+    static GLenum defaultUsageHint;
+
+    // Buffer bind target stacks
+    static ElementStack<Buffer> bindStack_GL_ARRAY_BUFFER;
+    static ElementStack<Buffer> bindStack_GL_ELEMENT_ARRAY_BUFFER;
+
+//====[METHODS]==============================================================//
+
+    // Return a reference to the selected bind target stack
+    static ElementStack<Buffer>& GetTargetStack(GLenum const bindTarget);
+
+  public:
+
+    // Initialise bind stacks
+    static void Init();
+
+    // Constructor, direct from GLuint
+    Buffer(GLuint const glHandle) : glHandle(glHandle) {}
+
+    // Loads data into the GL buffer
+    template <class T>
+    void SetData(
+      std::vector<T> const data,
+      GLenum const usageHint = defaultUsageHint) {
+
+      // Load data into buffer
+      glNamedBufferData(
+        this->glHandle,
+        sizeof(T) * data.size(),
+        (void*)data.data(),
+        usageHint);
+    }
+
+    // Constructor, loads data
+    template <class T>
+    Buffer(
+      std::vector<T> const data,
+      GLenum const usageHint = defaultUsageHint) {
+
+      glGenBuffers(1, &this->glHandle);
+      this->SetData<T>(data);
+    }
+
+    // Get the underlying opengl handle
+    GLuint GetGlHandle() {return this->glHandle;}
+
+    // Binding routines, selectable target (many different ways to use buffers)
+    void Bind(GLenum const bindTarget);
+    void Unbind(GLenum const bindTarget);
+
+    // Reference counted destructor
+    ~Buffer();
+  };
+
+} // namespace GLT
+
+#endif // GLT_BUFFER_INCLUDED
---
+++ src/util/Buffer.cpp
@@ -0,0 +1,90 @@
+#include <GLT/Buffer.hpp>
+using namespace GLT;
+
+
+// Standard
+#include <iostream>
+
+
+// Default buffer usage hint
+GLenum Buffer::defaultUsageHint = GL_STATIC_DRAW;
+
+
+// Bind target stacks
+ElementStack<Buffer> Buffer::bindStack_GL_ARRAY_BUFFER;
+ElementStack<Buffer> Buffer::bindStack_GL_ELEMENT_ARRAY_BUFFER;
+
+
+// Return a reference to the stack specified by
+ElementStack<Buffer>& Buffer::GetTargetStack(GLenum const bindTarget) {
+  switch(bindTarget) {
+    case GL_ARRAY_BUFFER:
+      return bindStack_GL_ARRAY_BUFFER;
+    case GL_ELEMENT_ARRAY_BUFFER:
+      return bindStack_GL_ELEMENT_ARRAY_BUFFER;
+    default:
+      std::cerr << "ERROR, Buffer bind target '";
+      std::cerr << bindTarget << "' not recognised\n";
+      exit(1);
+  }
+}
+
+
+// Initialise bind target stacks
+void Buffer::Init() {
+
+  // GL_ARRAY_BUFFER
+  bindStack_GL_ARRAY_BUFFER = ElementStack<Buffer>();
+  bindStack_GL_ARRAY_BUFFER.Push(Buffer(0));
+
+  // GL_ELEMENT_ARRAY_BUFFER
+  bindStack_GL_ELEMENT_ARRAY_BUFFER = ElementStack<Buffer>();
+  bindStack_GL_ELEMENT_ARRAY_BUFFER.Push(Buffer(0));
+}
+
+
+// Bind the buffer and remember the state of the current binding
+void Buffer::Bind(GLenum const bindTarget) {
+
+  // Get reference to apropriate bind stack
+  ElementStack<Buffer>& bindStack = GetTargetStack(bindTarget);
+
+  // If the currently bound buffer is not this one, bind this buffer
+  if(this->glHandle != bindStack.Top().GetGlHandle()) {
+    glBindBuffer(bindTarget, this->glHandle);
+  }
+
+  // Push this buffer onto the bind stack
+  bindStack.Push(*this);
+}
+
+
+// Unbind the buffer and restore the previous binding
+void Buffer::Unbind(GLenum const bindTarget) {
+
+  // Get reference to apropriate bind stack
+  ElementStack<Buffer>& bindStack = GetTargetStack(bindTarget);
+
+  // Can't unbind object that isn't currently bound without breaking stuff
+  if(this->glHandle != bindStack.Top().GetGlHandle()) {
+    std::cerr << "ERROR: Attempt to unbind already unbound buffer\n";
+    std::cerr << "Did you forget to call unbind?\n";
+    exit(1);
+  }
+
+  // Pop this object off the bind stack
+  bindStack.Pop();
+
+  // Restore the binding to what it was previously (if neccessary)
+  if(bindStack.Top().GetGlHandle() != this->glHandle) {
+    glBindBuffer(bindTarget, bindStack.Top().GetGlHandle());
+  }
+}
+
+
+// Reference counted destructor
+Buffer::~Buffer() {
+  if(!this->ReferencedElsewhere()) {
+    glDeleteBuffers(1, &this->glHandle);
+  }
+}
