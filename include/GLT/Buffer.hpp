694456d1d9af7b13372a15a9cb76709e4fc1d00a
ac101m <mofty101@gmail.com>
2019-07-28 19:20:09 +0100

    switch includes to quotes

--- include/GLT/Buffer.hpp
+++ include/GLT/Buffer.hpp
@@ -4,10 +4,10 @@
 
 // This project
 #ifndef GLT_MASTER_INCLUDED
-#include <GLT/Master.hpp>
+#include "GLT/Master.hpp"
 #endif
-#include <GLT/RefCount.hpp>
-#include <GLT/ElementStack.hpp>
+#include "GLT/RefCount.hpp"
+#include "GLT/ElementStack.hpp"
 
 
 // Standard
