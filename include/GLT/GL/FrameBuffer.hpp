6635d6f8e40220dfa07a60e3ce33cd18867cad9e
ac101m <mofty101@gmail.com>
2019-07-29 23:42:08 +0100

    Add framebuffer class

---
+++ include/GLT/GL/FrameBuffer.hpp
@@ -0,0 +1,65 @@
+#ifndef GLT_FRAMEBUFFER_INCLUDED
+#define GLT_FRAMEBUFFER_INCLUDED
+
+
+// Internal headers
+#ifndef GLT_GL_INCLUDED
+#include "GLT/Extern/GL.hpp"
+#endif
+#include "GLT/Util/RefCount.hpp"
+#include "GLT/GL/Texture.hpp"
+
+
+// Standard
+#include <vector>
+#include <memory>
+
+
+namespace GLT {
+
+  class FrameBuffer : public RefCount {
+  private:
+
+    // Opengl name
+    GLuint glName;
+
+    // Colour buffers
+    std::shared_ptr<std::vector<Texture>> colorBuffers;
+
+    // Bind stacks
+    static ElementStack<FrameBuffer> bindStack_GL_DRAW_FRAMEBUFFER;
+    static ElementStack<FrameBuffer> bindStack_GL_READ_FRAMEBUFFER;
+
+//====[METHODS]==============================================================//
+
+    // Return a reference to the selected bind target stack
+    static ElementStack<FrameBuffer>& GetTargetStack(GLenum const bindTarget);
+
+  public:
+
+    // Initialise bind stacks
+    static void Init();
+
+    // Constructor, direct from name
+    FrameBuffer(GLuint const glName = 0);
+
+    // Constructor, full
+    FrameBuffer(
+      unsigned const width,
+      unsigned const height,
+      std::vector<Texture> const colorBuffers);
+
+    // Return gl name
+    GLuint GetGlName() {return this->glName;}
+
+    // Bind and unbind
+    void Bind(GLenum const bindTarget);
+    void Unbind(GLenum const bindTarget);
+
+    // Reference counted destructor
+    ~FrameBuffer();
+  };
+
+} // namespace GLT
+
+#endif // GLT_FRAMEBUFFER_INCLUDED
---
+++ src/GL/FrameBuffer.cpp
@@ -0,0 +1,140 @@
+#include "GLT/GL/FrameBuffer.hpp"
+using namespace GLT;
+
+
+// Bind stacks
+ElementStack<FrameBuffer> FrameBuffer::bindStack_GL_DRAW_FRAMEBUFFER;
+ElementStack<FrameBuffer> FrameBuffer::bindStack_GL_READ_FRAMEBUFFER;
+
+
+// Initialise bind stacks
+void FrameBuffer::Init() {
+
+  // Draw framebuffer target
+  bindStack_GL_DRAW_FRAMEBUFFER.Clear();
+  bindStack_GL_DRAW_FRAMEBUFFER.Push(FrameBuffer(0));
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+  // Read framebuffer target
+  bindStack_GL_READ_FRAMEBUFFER.Clear();
+  bindStack_GL_READ_FRAMEBUFFER.Push(FrameBuffer(0));
+  glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+}
+
+
+// Get reference to apropriate bind stack
+ElementStack<FrameBuffer>& FrameBuffer::GetTargetStack(GLenum const bindTarget) {
+  switch(bindTarget) {
+    case GL_DRAW_FRAMEBUFFER:
+      return bindStack_GL_DRAW_FRAMEBUFFER;
+    case GL_READ_FRAMEBUFFER:
+      return bindStack_GL_READ_FRAMEBUFFER;
+    default:
+      std::cerr << "ERROR, Framebuffer bind target '";
+      std::cerr << bindTarget << "' not recognised\n";
+      exit(1);
+  }
+}
+
+
+// Constructor, direct from name
+FrameBuffer::FrameBuffer(GLuint const glName) {
+  this->glName = glName;
+  this->colorBuffers =
+    std::shared_ptr<std::vector<Texture>>(new std::vector<Texture>());
+}
+
+
+// Constructor, full
+FrameBuffer::FrameBuffer(
+  unsigned const width,
+  unsigned const height,
+  std::vector<Texture> const colorBuffers) {
+
+  // Construct framebuffer object
+  glGenFramebuffers(1, &this->glName);
+  this->Bind(GL_DRAW_FRAMEBUFFER);
+
+  // Intialise internal color buffer textures
+  this->colorBuffers =
+    std::shared_ptr<std::vector<Texture>>(new std::vector<Texture>());
+  *(this->colorBuffers) = colorBuffers;
+
+  // Add color buffers to textures
+  std::vector<GLuint> attachments;
+  for(unsigned i = 0; i < colorBuffers.size(); i++) {
+/*
+    glFramebufferTexture2D(
+      GL_FRAMEBUFFER,
+      GL_COLOR_ATTACHMENT0 + i,
+      GL_TEXTURE_2D,
+      this->textures[i].GetGlHandle(), 0);
+*/
+    attachments.push_back(GL_COLOR_ATTACHMENT0 + i);
+  }
+
+  this->Unbind(GL_DRAW_FRAMEBUFFER);
+}
+
+
+// Bind and record current binding
+void FrameBuffer::Bind(GLenum const bindTarget) {
+
+  // If the bind target is GL_FRAMEBUFFER, bind to both targets. This works
+  // because binding GL_DRAW_FRAMEBUFFER will also bind GL_FRAMEBUFFER
+  // At least from my experimentation.
+  if(bindTarget == GL_FRAMEBUFFER) {
+    this->Bind(GL_DRAW_FRAMEBUFFER);
+    this->Bind(GL_READ_FRAMEBUFFER);
+    return;
+  }
+
+  // Get reference to apropriate bind stack
+  ElementStack<FrameBuffer>& bindStack = GetTargetStack(bindTarget);
+
+  // If the currently bound buffer is not this one, bind this buffer
+  if(this->glName != bindStack.Top().GetGlName()) {
+    glBindFramebuffer(bindTarget, this->glName);
+  }
+
+  // Push this buffer onto the bind stack
+  bindStack.Push(*this);
+}
+
+
+// Unbind and restore previous binding
+void FrameBuffer::Unbind(GLenum const bindTarget) {
+
+  // If the bind target is GL_FRAMEBUFFER, unbind both targets separately
+  if(bindTarget == GL_FRAMEBUFFER) {
+    this->Unbind(GL_DRAW_FRAMEBUFFER);
+    this->Unbind(GL_READ_FRAMEBUFFER);
+    return;
+  }
+
+  // Get reference to apropriate bind stack
+  ElementStack<FrameBuffer>& bindStack = GetTargetStack(bindTarget);
+
+  // Can't unbind object that isn't currently bound without breaking stuff
+  if(this->glName != bindStack.Top().GetGlName()) {
+    std::cerr << "ERROR: Attempt to unbind already unbound framebuffer\n";
+    std::cerr << "Did you forget to call unbind?\n";
+    exit(1);
+  }
+
+  // Pop this object off the bind stack
+  bindStack.Pop();
+
+  // Restore the binding to what it was previously (if neccessary)
+  if(bindStack.Top().GetGlName() != this->glName) {
+    glBindFramebuffer(bindTarget, bindStack.Top().GetGlName());
+  }
+}
+
+
+// Reference counted destructor
+FrameBuffer::~FrameBuffer() {
+  if(!this->ReferencedElsewhere()) {
+    glDeleteFramebuffers(1, &this->glName);
+  }
+}
