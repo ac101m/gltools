d08c468463d1b55206ce3bbfb132ca6656fe03ae
ac101m <mofty101@gmail.com>
2019-07-30 17:38:30 +0100

    Add renderbuffer class

---
+++ include/GLT/GL/RenderBuffer.hpp
@@ -0,0 +1,61 @@
+#ifndef GLT_RENDERBUFFER_INCLUDED
+#define GLT_RENDERBUFFER_INCLUDED
+
+
+// This project
+#ifndef GLT_GL_INCLUDED
+#include "GLT/Extern/GL.hpp"
+#endif
+#include "GLT/Util/RefCount.hpp"
+#include "GLT/Util/ElementStack.hpp"
+
+
+namespace GLT {
+
+  class RenderBuffer : public RefCount {
+  private:
+
+    // Opengl assigned name
+    GLuint glName;
+
+    // Stack for renderbuffer bind target
+    static ElementStack<RenderBuffer> bindStack;
+
+  public:
+
+    // Initialise bind stack
+    static void Init();
+
+    // Void constructor, generate name but nothing else
+    RenderBuffer() {glGenRenderbuffers(1, &this->glName);}
+
+    // Construct directly from name
+    RenderBuffer(GLuint const glName) : glName(glName) {}
+
+    // Construct from parameters
+    RenderBuffer(
+      GLenum const format,
+      unsigned const width,
+      unsigned const height);
+
+    // Assign storage for render buffer
+    void Storage(
+      GLenum const format,
+      unsigned const width,
+      unsigned const height);
+
+    // Get opengl name
+    GLuint GetGlName() {return this->glName;}
+
+    // Bind and unbind
+    void Bind();
+    void Unbind();
+
+    // Reference counted destructor
+    ~RenderBuffer();
+  };
+
+} // namespace GLT
+
+
+#endif // GLT_RENDERBUFFER_INCLUDED
---
+++ src/GL/RenderBuffer.cpp
@@ -0,0 +1,79 @@
+#include "GLT/GL/RenderBuffer.hpp"
+using namespace GLT;
+
+
+// Stack for renderbuffer bind target
+ElementStack<RenderBuffer> RenderBuffer::bindStack;
+
+
+// Initialise bind stack
+void RenderBuffer::Init() {
+  bindStack.Clear();
+  bindStack.Push(RenderBuffer(0));
+  glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
+
+
+// Initialise from parameters
+RenderBuffer::RenderBuffer(
+  GLenum const format,
+  unsigned const width,
+  unsigned const height) {
+
+  // Generate a name and assign storage
+  glGenRenderbuffers(1, &this->glName);
+  this->Storage(format, width, height);
+}
+
+
+// Assign storage for render buffer
+void RenderBuffer::Storage(
+  GLenum const format,
+  unsigned const width,
+  unsigned const height) {
+
+  this->Bind();
+  glRenderbufferStorage(GL_RENDERBUFFER, format, width, height);
+  this->Unbind();
+}
+
+
+// Bind render buffer
+void RenderBuffer::Bind() {
+
+  // If the currently bound buffer is not this one, bind this buffer
+  if(this->glName != bindStack.Top().GetGlName()) {
+    glBindRenderbuffer(GL_RENDERBUFFER, this->glName);
+  }
+
+  // Push this buffer onto the bind stack
+  bindStack.Push(*this);
+}
+
+
+// Unbind render buffer
+void RenderBuffer::Unbind() {
+
+  // Can't unbind object that isn't currently bound without breaking stuff
+  if(this->glName != bindStack.Top().GetGlName()) {
+    std::cerr << "ERROR: Attempt to unbind already unbound renderbuffer\n";
+    std::cerr << "Did you forget to call unbind?\n";
+    exit(1);
+  }
+
+  // Pop this object off the bind stack
+  bindStack.Pop();
+
+  // Restore the binding to what it was previously (if neccessary)
+  if(bindStack.Top().GetGlName() != this->glName) {
+    glBindRenderbuffer(GL_RENDERBUFFER, bindStack.Top().GetGlName());
+  }
+}
+
+
+// Reference counted destructor
+RenderBuffer::~RenderBuffer() {
+  if(!this->ReferencedElsewhere()) {
+    glDeleteRenderbuffers(1, &this->glName);
+  }
+}
